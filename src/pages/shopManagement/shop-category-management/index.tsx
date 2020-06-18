377e4be6fc01f9d6e90542cf71084ce5c16f19dc
songyizhe <769724661@qq.com>
2020-06-18 15:56:36 +0800

    feat: 添加门店分类完成

--- src/pages/shopManagement/shop-category-management/index.tsx
+++ src/pages/shopManagement/shop-category-management/index.tsx
@@ -8,110 +8,127 @@ import { router } from "umi";
 const FormItem = Form.Item;
 
 interface Props {
-    form: any;
-    dispatch: (opt: any) => any;
-    shopCategoryList: any;
+  form: any;
+  dispatch: (opt: any) => any;
+  shopCategoryList: any;
 }
 
 export default Form.create()(
-    connect(({ shopCategoryList }: any) => ({ shopCategoryList }))(
-        class shopCategoryList extends Component<Props>{
+  connect(({ shopCategoryList }: any) => ({ shopCategoryList }))(
+    class shopCategoryList extends Component<Props>{
 
-            state = {
-                dataList: [],
-                loading: false,
-                total: 10,
-            }
+      state = {
+        dataList: [],
+        loading: false,
+        total: 10,
+      }
 
 
-            onSearch = async (e: any) => {
-                e.preventDefault();
-                let categoryName = this.props.form.getFieldValue('categoryName');
-                await this.props.dispatch({
-                    type: 'shopCategoryList/setSearchState',
-                    payload: {
-                        categoryName,
-                    },
-                });
-                // const { currentPage, currentPageSize } = this.props.shopCategoryList;
-                console.log(this.props)
-            }
+      onSearch = async (e: any) => {
+        e.preventDefault();
+        let categoryName = this.props.form.getFieldValue('categoryName');
+        await this.props.dispatch({
+          type: 'shopCategoryList/setSearchState',
+          payload: {
+            categoryName,
+          },
+        });
+        // const { currentPage, currentPageSize } = this.props.shopCategoryList;
+        console.log(this.props)
+      }
 
-            handleFormReset = async () => {
-                const { form, dispatch } = this.props;
-                form.resetFields();
-                await dispatch({
-                    type: 'shopCategoryList/resetFussySearch',
-                });
-            };
+      handleFormReset = async () => {
+        const { form, dispatch } = this.props;
+        form.resetFields();
+        await dispatch({
+          type: 'shopCategoryList/resetFussySearch',
+        });
+      };
 
-            render() {
-                const { getFieldDecorator } = this.props.form;
-                const { dataList, loading, total } = this.state;
-                const { currentPage, currentPageSize, categoryName } = this.props.shopCategoryList;
-                const columns = [
-                    {
-                        title: '序号',
-                        dataIndex: 'id',
-                        key: 'id',
-                        width: 100
-                    },
-                ]
-                return (
-                    <div>
-                        <div className={styles.tableListForm}>
-                            <Form layout="inline" onSubmit={this.onSearch.bind(this)}>
-                                <Row
-                                    gutter={{
-                                        md: 8,
-                                        lg: 24,
-                                        xl: 48,
-                                    }}
-                                >
-                                    <Col md={5} sm={20}>
-                                        <FormItem label='分类名称'>
-                                            {getFieldDecorator('categoryName', { initialValue: categoryName })(
-                                                <Input placeholder="请输入" />,
-                                            )}
-                                        </FormItem>
-                                    </Col>
-                                    <Col md={5} sm={26}>
-                                        <Button type="primary" htmlType="submit">
-                                            查询
+      handleAdd = () => {
+        router.push('/shopManagement/add-cate')
+      }
+
+      render() {
+        const { getFieldDecorator } = this.props.form;
+        const { dataList, loading, total } = this.state;
+        const { currentPage, currentPageSize, categoryName } = this.props.shopCategoryList;
+        const columns = [
+          {
+            title: '序号',
+            dataIndex: 'id',
+            key: 'id',
+            width: 100
+          },
+        ]
+        return (
+          <div>
+            <div className={styles.tableListForm}>
+              <Form layout="inline" onSubmit={this.onSearch.bind(this)}>
+                <Row
+                  gutter={{
+                    md: 8,
+                    lg: 24,
+                    xl: 48,
+                  }}
+                >
+                  <Col md={5} sm={20}>
+                    <FormItem label='分类名称'>
+                      {getFieldDecorator('categoryName', { initialValue: categoryName })(
+                        <Input placeholder="请输入" />,
+                      )}
+                    </FormItem>
+                  </Col>
+                  <Col md={5} sm={26}>
+                    <Button type="primary" htmlType="submit">
+                      查询
                                     </Button>
-                                        <Button
-                                            style={{
-                                                marginLeft: 8,
-                                            }}
-                                            onClick={this.handleFormReset}
-                                        >
-                                            重置
+                    <Button
+                      style={{
+                        marginLeft: 8,
+                      }}
+                      onClick={this.handleFormReset}
+                    >
+                      重置
                                     </Button>
-                                    </Col>
-                                </Row>
-                            </Form>
-                        </div>
 
-                        <Table
-                            rowKey="id"
-                            columns={columns}
-                            dataSource={dataList}
-                            loading={loading}
-                            // onChange={this.handleChange}
-                            pagination={{
-                                current: currentPage,
-                                defaultPageSize: currentPageSize,
-                                showSizeChanger: true,
-                                showQuickJumper: true,
-                                total,
-                                showTotal: () => {
-                                    return `共${total}条`;
-                                },
-                            }}
-                        />
-                    </div>
-                )
-            }
-        }
-    )
+                  </Col>
+                  <Col>
+                    <Button
+                      type="primary"
+                      style={{
+                        marginLeft: 8,
+                      }}
+                      onClick={this.handleAdd}
+                    >
+                      添加
+                                    </Button>
+                  </Col>
+
+                </Row>
+              </Form>
+            </div>
+
+            <Table
+              rowKey="id"
+              columns={columns}
+              dataSource={dataList}
+              loading={loading}
+              // onChange={this.handleChange}
+              pagination={{
+                current: currentPage,
+                defaultPageSize: currentPageSize,
+                showSizeChanger: true,
+                showQuickJumper: true,
+                total,
+                showTotal: () => {
+                  return `共${total}条`;
+                },
+              }}
+            />
+          </div>
+        )
+      }
+    }
+  )
 )
